f38f8c916147382f3d0be1bd3ae746beb7853e4c
4kari <45159811+4kari@users.noreply.github.com>
2022-02-19 21:11:38 +0700

    Add files via upload
    initial front-end

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
+
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<div id="loader">
+			<img src="assets/icon/logo-home.svg" class="rounded mx-auto d-block" alt="Cinque Terre" style="margin-top: 80px;">
+			<h3 class="display-3 text-center">SELAMAT DATANG DI BUKU</h3>
+			<h3 class="display-4 text-center">PENGHUBUNG BERBASIS</h3>
+			<h3 class="display-4 text-center"><i>MOBILE WEB</i></h3>
+</div>
+<script type="text/javascript">
+	$(document).ready(function() {
+		setTimeout(function(){$("#loader").remove();},1000);
+		window.location.href = "daftar.php";
+	});
+
+
+</script>
+<script type="text/javascript"></script>
+</html>
