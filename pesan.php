07b2adf69d648342ae951e9c8d98f314d459877e
danihartanto <48488561+danihartanto@users.noreply.github.com>
2022-02-28 10:00:19 +0700

    FE perpesanan

---
+++ pesan.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kotak Masuk</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template-->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="assets/css/style-tulis-pesan.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-light">
+    <nav class="navbar navbar-expand-sm bg-light justify-content-right fixed-top">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="kotak-masuk.php"><i class="fa fa-arrow-left" style="font-size:24px"> Back</i><span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card chat-app">
+                    <div class="chat">
+                        <div class="chat-header clearfix">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
+                                        <img src="assets/img/avatar-cewek.png" alt="avatar">
+                                    </a>
+                                    <div class="chat-about">
+                                        <h6 class="m-b-0">Wali Murid</h6>
+                                        <small>ID Chat: 3321123</small>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="chat-history">
+                            <ul class="m-b-0">
+                                <li class="clearfix">
+                                    <div class="message-data text-right">
+                                        <span class="message-data-time">10:10, 28/02/2022</span>
+                                        <img src="assets/img/avatar-profil.png" alt="avatar">
+                                    </div>
+                                    <div class="message other-message float-right"> Selamat pagi bu, saya ingin menginatkan bahwasanya ananda ramadhan
+                                        belum mengumpulkan tugas tema 3 pada halaman 39 bu </div>
+                                </li>
+                                <li class="clearfix">
+                                    <div class="message-data">
+                                        <span class="message-data-time">10:12, 28/02/2022</span>
+                                    </div>
+                                    <div class="message my-message">Baik bu, akan saya bimbing anak saya untuk bisa segera mengerjakan, terima kasih sudah diingatkan bu.</div>
+                                </li>
+                                <li class="clearfix">
+                                    <div class="message-data">
+                                        <span class="message-data-time">10:15, 28/02/2022</span>
+                                    </div>
+                                    <div class="message my-message">Project has been already finished and I have results to show you.</div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="chat-message clearfix">
+                            <div class="form-row mb-4">
+                                <div class="col col-12">
+                                    <textarea rows="1" placeholder="Tulis pesan disini..." class="form-control"></textarea>
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-success btn-sm float-right mt-1">
+                                        <i class="fa fa-paper-plane"></i>
+                                        kirim
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bootstrap core JavaScript-->
+        <script src="assets/vendor/jquery/jquery.min.js"></script>
+        <script src="assetsvendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Core plugin JavaScript-->
+        <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+        <!-- Custom scripts for all pages-->
+        <script src="assets/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
