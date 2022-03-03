1d6c529ffc755666ad7b7dda4a983b48cd44abbe
4kari <mypasshidden@gmail.com>
2022-03-04 01:01:27 +0700

    fitur mulai chat

--- backend/mulai_chat.php
+++ backend/mulai_chat.php
@@ -3,27 +3,28 @@
 require 'connect.php';
 
 session_start();
-$username = $_SESSION['user'];
-$user2 = $_get['username'];
-
+$username = $_SESSION['username'];
+$user2 = $_GET['username'];
 //menambahkan pesan ke tabel pesan berdasarkan id_chat
 $sql    = "INSERT INTO chat (id_guru, id_wali) VALUES (:id_guru, :id_wali)";
 $query  = $conn->prepare($sql);
 $id=0;
 if($_SESSION['level']==1){
-    $query->execute(array(
-        ':id_guru' => $username,
-        ':id_wali' => $user2,
-    ));
-    $id = $conn->query("SELECT * FROM chat WHERE id_guru = '$username' and id_wali = '$user2'" , PDO::FETCH_ASSOC)->fetch()['id'];
+    $id_wali = $conn->query("SELECT * FROM wali WHERE username = '$user2'" , PDO::FETCH_ASSOC)->fetch()['id'];
+    // $query->execute(array(
+    //     ':id_guru' => $username,
+    //     ':id_wali' => $id_wali,
+    // ));
+    $id = $conn->query("SELECT * FROM chat WHERE id_guru = '$username' and id_wali = '$id_wali'" , PDO::FETCH_ASSOC)->fetch()['id'];
 }else{
-    $query->execute(array(
-        ':id_guru' => $user2,
-        ':id_wali' => $username,
-    ));
-    $id = $conn->query("SELECT * FROM chat WHERE id_guru = '$user2' and id_wali = '$username'" , PDO::FETCH_ASSOC)->fetch()['id'];
+    $id_wali = $conn->query("SELECT * FROM wali WHERE username = '$username'" , PDO::FETCH_ASSOC)->fetch()['id'];
+    // $query->execute(array(
+    //     ':id_guru' => $user2,
+    //     ':id_wali' => $id_wali
+    // ));
+    $id = $conn->query("SELECT * FROM chat WHERE id_guru = '$user2' and id_wali = '$id_wali'" , PDO::FETCH_ASSOC)->fetch()['id'];
 }
-header("location:../pesan.php/?id=".$id);
+header("location:../../pesan.php/?id=".$id);
 //=======================//
 
 ?>
