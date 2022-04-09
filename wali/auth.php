4d61dea07f536494f138b849df3ad60ced6143e8
danihartanto <48488561+danihartanto@users.noreply.github.com>
2022-04-09 15:11:04 +0700

    Delete wali directory

--- wali/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'koneksi.php';
-session_start();
-if($_SESSION['level']!=2){
-    header('location:../backend/auth.php');
-}
-
-?>
--- wali/load_data.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require 'koneksi.php';
-require 'auth.php';
-$data = mysqli_query($koneksi, "SELECT * FROM wali WHERE username='$username'");
-while ($result = mysqli_fetch_array($data)) {
-    $id = $result['id'];
-}
-
-?>
